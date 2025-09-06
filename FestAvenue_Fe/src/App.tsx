d72fa0089b7fa04a4429e5b2403471bcfe587e6a
vuquangminhpe <minhvqhe176726@fpt.edu.vn>
2025-09-06 17:45:51 +0700

    Update_api login google

--- FestAvenue_Fe/src/App.tsx
+++ FestAvenue_Fe/src/App.tsx
@@ -1,7 +1,21 @@
+'use client'
+import { useEffect } from 'react'
 import './App.css'
 import useRouteElement from './useRouteElement'
-
+import { useLocation } from 'react-router'
+import { saveAccessTokenToLS } from './utils/auth'
 function App() {
+  const location = useLocation()
+  const check_accessToken = location.search.includes('accessToken')
+  console.log(location)
+
+  useEffect(() => {
+    if (check_accessToken) {
+      const accessToken = location.search.split('=')[2]
+      saveAccessTokenToLS(accessToken)
+    }
+  }, [check_accessToken, location.search])
+
   const routeElement = useRouteElement()
   return <>{routeElement}</>
 }
--- FestAvenue_Fe/src/pages/User/Public/Home/Home.tsx
+++ FestAvenue_Fe/src/pages/User/Public/Home/Home.tsx
@@ -1,3 +1,16 @@
+import { getAccessTokenFromLS } from '@/utils/auth'
+import * as signalR from '@microsoft/signalr'
 export default function Home() {
+  const connection = new signalR.HubConnectionBuilder()
+    .withUrl('https://hoalacrent.io.vn/chathub', {
+      accessTokenFactory: () => getAccessTokenFromLS() || ''
+    })
+    .build()
+
+  connection.on('send', (data) => {
+    console.log(data)
+  })
+
+  connection.start().then(() => connection.invoke('send', 'Hello'))
   return <div>Home</div>
 }
