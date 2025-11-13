76df773847a0c4c789dbc8ead817d52a91d6845a
vuquangminhpe <minhvqhe176726@fpt.edu.vn>
2025-11-13 16:46:22 +0700

    add scroll to top and fix event types

--- FestAvenue_Fe/src/main.tsx
+++ FestAvenue_Fe/src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router'
 import { HelmetProvider } from 'react-helmet-async'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from '@/components/ui/sonner'
+import ScrollToTop from './components/custom/ScrollToTop/ScrollToTop.tsx'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,6 +21,7 @@ createRoot(document.getElementById('root')!).render(
   <HelmetProvider>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
+        <ScrollToTop />
         <App />
         <Toaster />
       </QueryClientProvider>
